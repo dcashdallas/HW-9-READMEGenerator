2f077fe98a8e2cb947db5d9af6146b47089907ec
dcashdallas <dcashdallas81@yahoo.com>
2020-05-28 18:08:56 -0500

    adding questions for generator

--- index.js
+++ index.js
@@ -1,12 +1,69 @@
+const fs = require("fs");
+// const axios = require("axios"); not needed 
+const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown.js");
+
+
+
 const questions = [
+    //ask user for name of the app 
+    {
+
+        type: 'input',
+        name: 'repoName',
+        message: 'What is name of this application? (This will aslo the main header of the README)',
+
+    },
+    //asks user for git hub user name 
+    {
+        type: 'input',
+        name: 'gitHubName',
+        message: 'What is your username on Github?',
+
+    },
+    // asks for a description of the project 
+    {
+        type: 'input',
+        message: 'Write a description of this application',
+        name: 'description',
+
+    },
+    // How do you install your app? 
+    {
+        type: 'input',
+        message: 'How does a user install your application? Provide instuctions here',
+        name: 'installApp',
+
+    },
+    // How does a user use your application? 
+    {
+        type: 'input',
+        message: 'How does a user use your application? Provide instuctions here',
+        name: 'appInstructions',
+
+    },
+    // How would a user of this application report an issue, or make a contribution to it? 
+    {
+        type: 'input',
+        message: 'Would you like you users to be able to report issues or contribute to this project? add instructions here.',
+        name: 'appIssuesAndCont',
+
+    },
+    // promts what license they would you  like to use? 
+    // THIS SHOULD BE USED FOR A BADGE. 
+    {
+        type: 'input',
+        message: 'What license they would you  like to use? (Ex: MIT, GPL 3.0) If you do not wish to use a license type "none" ',
+        name: 'license',
+
+    },
+    //Asks the user who else contributed to their application 
+    {
+        type: 'input',
+        message: 'Who else contributed to your application? if none write "none" ',
+        name: 'contributors',
+
+    },
 
 ];
 
-function writeToFile(fileName, data) {
-}
-
-function init() {
-
-}
-
-init();
