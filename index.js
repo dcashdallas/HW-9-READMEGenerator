9c98000c32df765e75c36bc0950dc602914aac45
dcashdallas <dcashdallas81@yahoo.com>
2020-05-30 21:50:20 -0500

    updating additional details

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 // const axios = require("axios"); not needed 
 const inquirer = require("inquirer");
-// const generateMarkdown = require("./utils/generateMarkdown.js");
-
+// const generateMarkdown = require("utils/generateMarkdown.js");
+const api = require("utils/api.js")
 
 
 const questions = [
@@ -67,4 +67,118 @@ const questions = [
 
 ];
 
+function init() {
+    inquirer.prompt(questions).then((response) => {
+
+        // this will Create the first line and main header for the repo (that is what the "# " is for) using the users resonse to RepoName
+        fs.appendFileSync("README.md", ("# " + response.repoName) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        // this will create the 2nd line of readme discribing the application was developed by the users reponse to githubname 
+        fs.appendFileSync("README.md", ("This application was developed by: " + response.gitHubName + '\n') + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+
+        // adds the discription of the project that the users entered 
+        fs.appendFileSync("README.md", (response.description) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        //creates a sub header for installation and adds the instructions the users entered 
+        fs.appendFileSync("README.md", ("## Installation" + '\n' + response.installApp) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        // creates a sub header for App usage and adds the instructions the users entered 
+        fs.appendFileSync("README.md", ("## How to use the Application" + '\n' + response.appInstructions) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        // creates a sub header for issue reporting and contriubiting and adds the instructions the users entered 
+        fs.appendFileSync("README.md", ("## Issue Reporting and Contributing" + '\n' + response.appIssuesAndCont) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        // creates a sub header for issue reporting and contriubiting and adds the instructions the users entered 
+        fs.appendFileSync("README.md", ("## Other Contibuting Developers:" + '\n' + response.contributors) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        // creates a sub header for licences and adds the licences the users entered 
+        fs.appendFileSync("README.md", ("## Licence(s)" + '\n' + response.license) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+
+    })
+}
+
+//runs the function to creat the file 
+init();
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//for testing purposes: 
+
+//Shows waht the resposne for the questions were. 
+// console.log(response)
+// console.log(response.gitHubName)
+// console.log(response.repoName)
+// console.log(response.description)
+// console.log(response.appInstructions)
+// console.log(response.appIssuesAndCont)
+// console.log(response.license)
+// console.log(response.contributors)
 
