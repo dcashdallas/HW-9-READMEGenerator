b98e47d022239643ad45fd93ae971c5a45108076
dcashdallas <dcashdallas81@yahoo.com>
2020-06-08 22:56:43 -0500

    updating files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# HW-9-READMEGenerator
-README Generator
+# Readme Generator
+This application was developed by: dcashdallas
+
+A simple Readme File Generator
+## Installation
+Run through node on the terminal
+## How to use the Application
+Run through terminal and fill out the prompts
+## Issue Reporting and Contributing
+N
+## Other Contibuting Developers:
+none
+## Licence(s)
+MIT
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
-// const axios = require("axios"); not needed 
+
 const inquirer = require("inquirer");
-// const generateMarkdown = require("utils/generateMarkdown.js");
-const api = require("utils/api.js")
+
 
 
 const questions = [
-    //ask user for name of the app 
+
     {
 
         type: 'input',
@@ -14,50 +13,48 @@ const questions = [
         message: 'What is name of this application? (This will aslo the main header of the README)',
 
     },
-    //asks user for git hub user name 
     {
         type: 'input',
         name: 'gitHubName',
         message: 'What is your username on Github?',
 
     },
-    // asks for a description of the project 
+
     {
         type: 'input',
         message: 'Write a description of this application',
         name: 'description',
 
     },
-    // How do you install your app? 
+
     {
         type: 'input',
         message: 'How does a user install your application? Provide instuctions here',
         name: 'installApp',
 
     },
-    // How does a user use your application? 
+
     {
         type: 'input',
         message: 'How does a user use your application? Provide instuctions here',
         name: 'appInstructions',
 
     },
-    // How would a user of this application report an issue, or make a contribution to it? 
+
     {
         type: 'input',
         message: 'Would you like you users to be able to report issues or contribute to this project? add instructions here.',
         name: 'appIssuesAndCont',
 
     },
-    // promts what license they would you  like to use? 
-    // THIS SHOULD BE USED FOR A BADGE. 
+
     {
         type: 'input',
         message: 'What license they would you  like to use? (Ex: MIT, GPL 3.0) If you do not wish to use a license type "none" ',
         name: 'license',
 
     },
-    //Asks the user who else contributed to their application 
+
     {
         type: 'input',
         message: 'Who else contributed to your application? if none write "none" ',
@@ -70,7 +67,7 @@ const questions = [
 function init() {
     inquirer.prompt(questions).then((response) => {
 
-        // this will Create the first line and main header for the repo (that is what the "# " is for) using the users resonse to RepoName
+
         fs.appendFileSync("README.md", ("# " + response.repoName) + '\n', function (err) {
 
             if (err) {
@@ -81,7 +78,7 @@ function init() {
             }
 
         })
-        // this will create the 2nd line of readme discribing the application was developed by the users reponse to githubname 
+
         fs.appendFileSync("README.md", ("This application was developed by: " + response.gitHubName + '\n') + '\n', function (err) {
 
             if (err) {
@@ -93,7 +90,7 @@ function init() {
 
         })
 
-        // adds the discription of the project that the users entered 
+
         fs.appendFileSync("README.md", (response.description) + '\n', function (err) {
 
             if (err) {
@@ -104,7 +101,7 @@ function init() {
             }
 
         })
-        //creates a sub header for installation and adds the instructions the users entered 
+
         fs.appendFileSync("README.md", ("## Installation" + '\n' + response.installApp) + '\n', function (err) {
 
             if (err) {
@@ -115,7 +112,7 @@ function init() {
             }
 
         })
-        // creates a sub header for App usage and adds the instructions the users entered 
+
         fs.appendFileSync("README.md", ("## How to use the Application" + '\n' + response.appInstructions) + '\n', function (err) {
 
             if (err) {
@@ -126,7 +123,7 @@ function init() {
             }
 
         })
-        // creates a sub header for issue reporting and contriubiting and adds the instructions the users entered 
+
         fs.appendFileSync("README.md", ("## Issue Reporting and Contributing" + '\n' + response.appIssuesAndCont) + '\n', function (err) {
 
             if (err) {
@@ -137,7 +134,7 @@ function init() {
             }
 
         })
-        // creates a sub header for issue reporting and contriubiting and adds the instructions the users entered 
+
         fs.appendFileSync("README.md", ("## Other Contibuting Developers:" + '\n' + response.contributors) + '\n', function (err) {
 
             if (err) {
@@ -148,7 +145,7 @@ function init() {
             }
 
         })
-        // creates a sub header for licences and adds the licences the users entered 
+
         fs.appendFileSync("README.md", ("## Licence(s)" + '\n' + response.license) + '\n', function (err) {
 
             if (err) {
@@ -163,22 +160,9 @@ function init() {
     })
 }
 
-//runs the function to creat the file 
+
 init();
 
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-//for testing purposes: 
-
-//Shows waht the resposne for the questions were. 
-// console.log(response)
-// console.log(response.gitHubName)
-// console.log(response.repoName)
-// console.log(response.description)
-// console.log(response.appInstructions)
-// console.log(response.appIssuesAndCont)
-// console.log(response.license)
-// console.log(response.contributors)
 
